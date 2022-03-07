7eab3d1a03b52599e7f0c8764b7ac03e73ba8d06
Sushil1204 <sushilspundkar@gmail.com>
2022-03-07 12:40:28 +0530

    Homepage with carousel and coin table is created

---
+++ crypto-club/src/App.js
@@ -0,0 +1,31 @@
+import './App.css';
+import {BrowserRouter} from "react-router-dom"
+import Header from "./Components/Header"
+import { Route } from 'react-router-dom';
+import Homepage from './Pages/Homepage';
+import CoinPage from './Pages/CoinPage';
+import {makeStyles} from "@material-ui/core"
+
+function App() {
+
+  const useStyles = makeStyles(() => ({
+    App: {
+      backgroundColor: '#14161a',
+      color: 'white',
+      minHeight:"100vh",
+     }
+  }));
+
+  const classes = useStyles()
+  return (
+    <BrowserRouter>
+      <div className={classes.App}>
+        <Header />
+        <Route path="/"component={Homepage} exact/>
+        <Route path="/coins/:id"component={CoinPage}/>
+      </div>
+    </BrowserRouter>
+  );
+}
+
+export default App;
